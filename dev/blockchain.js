6342ca1fb97a4d589901b6b76d999e602f42ceff
Vibhor Nautiyal <vibhor.nautiyal@hashedin.com>
2021-09-21 20:22:46 +0530

    Initial commit- Project Blockchain

---
+++ dev/blockchain.js
@@ -0,0 +1,42 @@
+function Blockchain(){
+    this.chain=[];
+    this.pendingTransactions=[];
+}
+
+Blockchain.prototype.createNewBlock=function(nonce,previousBlockHash,hash){
+
+    const newBlock={
+
+        index: this.chain.length+1,
+        timestamp: Date.now(),
+        transactions: this.pendingTransactions,
+        nonce:nonce,
+        hash:hash,
+        previousBlockHash:previousBlockHash
+    };
+    this.chain.push(newBlock);
+    this.pendingTransactions=[];
+    return newBlock;
+}
+
+Blockchain.prototype.getLastBlock=function(){
+
+    return this.chain[this.chain.length-1];
+}
+
+Blockchain.prototype.createNewTransaction=function(amount,sender,recipient){
+
+    const newTransaction={
+        amount:amount,
+        sender:sender,
+        recipient:recipient
+    }
+
+    this.pendingTransactions.push(newTransaction);
+
+    return this.getLastBlock()['index']+1;
+
+}
+
+
+module.exports=Blockchain;
---
+++ dev/test.js
@@ -0,0 +1,11 @@
+const Blockchain=require("./blockchain");
+
+const bitcoin=new Blockchain();
+
+bitcoin.createNewBlock(1234,"wkfh34fq43ijfoiwe","eieuffhnweichscejwc4");
+
+bitcoin.createNewTransaction(100,"0xfnjngjrnreknf","0xjrnjgbenwkr");
+
+bitcoin.createNewBlock(4567,"0xhrekenrervken","0xuihgnguh4i4h48tn");
+
+console.log(bitcoin);
